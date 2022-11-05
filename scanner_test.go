97d14156009246c2b57cab889ff42599951dffe9
Jack HUANG <jiajue.huang@live.com>
2022-11-05 10:53:58 +0800

    add more tests

--- scanner_test.go
+++ scanner_test.go
@@ -9,48 +9,60 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestSplit(t *testing.T) {
-	Convey("It can split by newline", t, func() {
+func TestHappyFlow(t *testing.T) {
+	Convey("it can split by newline", t, func() {
 		fd, err := ioutil.TempFile(os.TempDir(), "scanner_test")
 		So(err, ShouldBeNil)
 		defer fd.Close()
 
-		rfd, err := os.Open(fd.Name())
-		So(err, ShouldBeNil)
-
-		s := Scanner{
-			FD:    rfd,
-			Buf:   make([]byte, 1024),
-			Split: SplitLines,
-		}
-
 		fd.WriteString("hello\nworld\n")
 		fd.Sync()
 
-		hasNext := s.Scan()
-		So(hasNext, ShouldBeTrue)
-		So(s.Err(), ShouldBeNil)
+		testScannerWithBufSize := func(n int) {
+			rfd, err := os.Open(fd.Name())
+			So(err, ShouldBeNil)
 
-		chunk := s.Chunk()
-		So(chunk, ShouldResemble, Chunk{
-			Start: 0,
-			End:   6,
-			Raw:   []byte("hello\n"),
+			s := Scanner{
+				FD:    rfd,
+				Buf:   make([]byte, n),
+				Split: SplitLines,
+			}
+
+			hasNext := s.Scan()
+			So(hasNext, ShouldBeTrue)
+			So(s.Err(), ShouldBeNil)
+
+			chunk := s.Chunk()
+			So(chunk, ShouldResemble, Chunk{
+				Start: 0,
+				End:   6,
+				Raw:   []byte("hello\n"),
+			})
+
+			hasNext = s.Scan()
+			So(hasNext, ShouldBeTrue)
+			So(s.Err(), ShouldBeNil)
+
+			chunk = s.Chunk()
+			So(chunk, ShouldResemble, Chunk{
+				Start: 6,
+				End:   12,
+				Raw:   []byte("world\n"),
+			})
+
+			hasNext = s.Scan()
+			So(hasNext, ShouldBeFalse)
+			So(s.Err(), ShouldEqual, io.EOF)
+		}
+
+		Convey("when the buffer is smaller than the file", func() {
+			testScannerWithBufSize(10)
 		})
-
-		hasNext = s.Scan()
-		So(hasNext, ShouldBeTrue)
-		So(s.Err(), ShouldBeNil)
-
-		chunk = s.Chunk()
-		So(chunk, ShouldResemble, Chunk{
-			Start: 6,
-			End:   12,
-			Raw:   []byte("world\n"),
+		Convey("when the buffer is bigger than the file", func() {
+			testScannerWithBufSize(1024)
 		})
-
-		hasNext = s.Scan()
-		So(hasNext, ShouldBeFalse)
-		So(s.Err(), ShouldEqual, io.EOF)
+		Convey("when the buffer is same size as the file", func() {
+			testScannerWithBufSize(12)
+		})
 	})
 }
